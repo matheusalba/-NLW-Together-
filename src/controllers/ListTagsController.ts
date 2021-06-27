c86d7f6dc1f48a241f4da05eed2007ffce83c8bc
matheusalba <matheuscampos306@gmail.com>
2021-06-26 21:53:28 -0300

    nlw

---
+++ src/controllers/ListTagsController.ts
@@ -0,0 +1,15 @@
+import {Request, Response} from "express";
+import { ListTagsService } from "../services/listTagsService";
+
+class ListTagsController{
+    async handle(request:Request,response:Response){
+        const listTagsService = new ListTagsService();
+
+        const tags = await listTagsService.execute();
+
+        return response.json(tags);
+
+    }
+}
+
+export { ListTagsController};
